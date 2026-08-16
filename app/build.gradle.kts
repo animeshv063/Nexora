7c65da462529d6bb1d09204c3609a00c7de1147c
Animesh Verma <animeshv063@gmail.com>
2026-08-16 19:54:22 +0530

    Added files for backend and frontend

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,8 +1,9 @@
 plugins {
     alias(libs.plugins.android.application)
+    alias(libs.plugins.kotlin.android)
     alias(libs.plugins.kotlin.compose)
-
     id("com.google.devtools.ksp")
+    id("com.google.dagger.hilt.android")
     id("org.jetbrains.kotlin.plugin.serialization")
     alias(libs.plugins.google.gms.google.services)
 }
